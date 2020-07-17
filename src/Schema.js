038a9a1fd7319752f4e494d46ca2298f005bc84a
gabiseabra <gabriela.lima.se@gmail.com>
2020-07-17 13:49:28 -0300

    decoding functions

--- src/Schema.js
+++ src/Schema.js
@@ -1,6 +1,8 @@
 const {
   getPath,
   getIn,
+  setIn,
+  reduce,
   mapBoth,
   mapKeys,
   mapValues,
@@ -12,7 +14,7 @@ const id = (x) => x
 const last = (x) => x.slice(-1)
 
 const mapEncodedKey = (key, {src = id}) => src(key)
-const mapEncodedValue = (obj) => (key, {encode = id}) => encode(obj[key], obj)
+const mapEncodedValue = (obj) => (key, {encode = id}) => encode(obj[key])
 
 const encodeKey = (schema) => ($path) => {
   const path = getPath($path)
@@ -30,19 +32,33 @@ const encodeValues = (schema) => (obj) =>
 const encode = (schema) => (obj) =>
   mapBoth(mapEncodedKey, mapEncodedValue(obj))(schema)
 
+const decodeKeys = (schema) => (obj) =>
+  reduce((acc, key, {encode = id, src = id}) => {
+    const srcKey = src(key)
+    return setIn(acc, key, encode(getIn(obj, srcKey)))
+  }, {})(schema)
+
+const decodeValues = (schema) => (obj) =>
+  reduce((acc, key, {decode = id, src = id}) => {
+    const srcKey = src(key)
+    return setIn(acc, srcKey, decode(getIn(obj, srcKey)))
+  }, {})(schema)
+
 const decode = (schema) => (obj) =>
-  mapValues((key, {decode = id, src = id}) =>
-    decode(getIn(obj, src(key)), obj)
-  )(schema)
+  mapValues((key, {decode = id, src = id}) => decode(getIn(obj, src(key))))(
+    schema
+  )
 
 const normalize = (schema) => (obj) =>
-  mapValues((key, {normalize = id}) => normalize(obj[key]), obj)(schema)
+  mapValues((key, {normalize = id}) => normalize(obj[key]))(schema)
 
 module.exports = {
   encodeKey,
   encodeKeys,
   encodeValues,
   encode,
+  decodeKeys,
+  decodeValues,
   decode,
   normalize
 }
